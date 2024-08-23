b974d913e0d858b54acb9f404e123648993292cd
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-23 15:52:33 +0000

    django changes

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k_=+o=ky&11n8c@dcic-+n99c+9p!f+gdc=*u7*ywiwr(1izga
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'codestar',
+    'blog',
 ]
 
 MIDDLEWARE = [
