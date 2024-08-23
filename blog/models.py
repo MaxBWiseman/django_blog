f8d50986fdbb59b83cc5a90ad16e43b508283381
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-23 17:48:41 +0000

    feat: Add 'exerpt' field to the Post model

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    exerpt = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
