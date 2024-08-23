86fd24176de427dc69539dac93b285f46f144f50
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-23 16:24:07 +0000

    chore: Update DEBUG setting to False in settings.py and configure database connection

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k_=+o=ky&11n8c@dcic-+n99c+9p!f+gdc=*u7*ywiwr(1izga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
@@ -84,6 +84,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
