c7d7c22a1b4727ac552f094a41ac61291e6f5391
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-26 13:30:10 +0000

    feat: Add Comment model to the blog app

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+# above imports the User model from django.contrib.auth.models as its built in to Django
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -18,3 +19,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
