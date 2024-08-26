28f5650963ba9eb7f3b14f037cfb42813e0aa512
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-26 09:45:20 +0000

    chore: Add 'updated_on' field to the Post model

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     date_posted = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     exerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
