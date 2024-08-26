9c225b27ff380398dbf80b4cf23a08ee6db92611
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-26 18:27:16 +0000

    chore: Update models comments

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # above imports the User model from django.contrib.auth.models as its built in to Django
 
 STATUS = ((0, "Draft"), (1, "Published"))
+# status choices for the Post model
 
 # Create your models here.
 class Post(models.Model):
@@ -11,9 +12,14 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts'
         )
+    # The author field is a foreign key that links to the User model. The on_delete parameter
+    # specifies the behavior to adopt when the referenced object is deleted. CASCADE specifies
+    # that when the user is deleted, also delete the blog posts associated with that user.
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    # This field uses the choices parameter to limit the available options for the status field to Draft
+    # and Published.
     exerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     
--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
+    # This line of code tells Django to retrieve all posts with a status of 1 (published) and order them
+    # by the created_on field in descending order.
     template_name = "post_list.html"
