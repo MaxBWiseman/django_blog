3863c172ca982fe3f7ac2ebfec6a818b7249eae7
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-27 13:53:30 +0000

    chore: added summernote app and re ordered urls

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1).order_by("-date_posted")
     # This line of code tells Django to retrieve all posts with a status of 1 (published) and order them
     # by the created_on field in descending order.
     template_name = "post_list.html"
