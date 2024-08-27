2f7fae7c699336827e14cb63bbf5c2df27597b6e
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-27 16:07:52 +0000

    chore: Update blog templates and views for improved post listing and display

--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,5 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-date_posted")
     # This line of code tells Django to retrieve all posts with a status of 1 (published) and order them
     # by the created_on field in descending order.
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
