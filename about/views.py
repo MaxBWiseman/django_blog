797265b9f8c518986cabb307662f70d838f2c3ea
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-31 19:57:21 +0000

    chore: Update collaborate form handling in about_me view

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateRequestForm
 
@@ -23,8 +24,26 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
 # This retrieves the most recent About object from the database
-    collaborate_form = CollaborateRequestForm()
-# This creates an instance of the CollaborateRequestForm form
+  
+    if request.method == "POST":
+# The first argument sent to any Django view function is the request object. Convention states that we give this parameter the name of request as well, for example:
+# def about_me(request):
+# That means that we can determine the HTTP verb that was used for our request by looking at the request.method property.
+        print("Recieved a POST request")
+        collaborate_form = CollaborateRequestForm(data=request.POST)
+        if collaborate_form.is_valid():
+# The is_valid() method makes sure we don't try to write a null value to the database. It also helps improve the security of our system
+            collaborate_form.save()
+# Now, we can finally call the save method to write the data to the database.
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Request submitted and I aim to respond within 24 hours.'
+            )
+#  When a prompt message is added, we then display it using the code we added below the nav in base.html.
+    
+
+    else:
+        collaborate_form = CollaborateRequestForm()
 
     return render(
         request,
